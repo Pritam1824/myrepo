80c0e97cc922807d51bbfa9c838080de806eeb46
Pritam gunjawate <pritamgunjawate100@gmail.com>
2022-05-27 17:13:06 +0530

    demo.java created

---
+++ demo.java
@@ -0,0 +1,22 @@
+ass Date{
+	    private int day;
+	        private int month;
+		    private int year;
+
+		        public Date(int day, int month, int year) {
+				        this.day = day;
+					        this.month = month;
+						        this.year = year;
+							    }
+
+			    public String toString() {
+				            return (day +" "+month+" "+year);
+					        }
+}
+public class Program {
+	    public static void mainStrg[] args) {
+		            Daete = null; //new Date(22,03,1996)
+			            Sstem.out.println(date.to(int dayString());
+						        }
+}
+
